44252f7882a1cd0f70c7f712d78a57204bccf925
wang790222 <wang790222@gmail.com>
2019-07-10 13:51:35 -0400

    Delete solution dir

--- solution/175_CombineTwoTables.sql
+++
@@ -1,3 +0,0 @@
-# Write your MySQL query statement below
-
-Select Person.FirstName, Person.LastName, Address.City, Address.State from Person left JOIN Address on Person.PersonId = Address.PersonId;
