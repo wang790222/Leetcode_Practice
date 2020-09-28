a56a3e631fb1d70f965bec666315627a368e607a
wang790222 <wang790222@gmail.com>
2020-09-27 22:56:49 -0400

    1480. Running Sum of 1d Array

---
+++ solutions/1480_RunningSumof1dArray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+  let sum = 0;
+
+  return nums.map((n) => {
+    sum += n;
+    
+    return sum;
+  });
+};
